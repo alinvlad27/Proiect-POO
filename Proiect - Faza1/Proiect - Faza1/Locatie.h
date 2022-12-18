711a2901892593bc95e2622781a4e3d78a2553f0
alinvlad27 <120591096+alinvlad27@users.noreply.github.com>
2022-12-18 21:39:13 +0200

    Add files via upload

---
+++ Proiect - Faza1/Proiect - Faza1/Locatie.h
@@ -0,0 +1,287 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Locatie
+{
+private:
+	char* adresa;
+	int nrMaximDeLocuri;
+	int nrLocuriOcupate;
+public:
+	// constructori
+	Locatie()
+	{
+		this->adresa = new char[strlen("Nedefinita") + 1];
+		strcpy(this->adresa, "Nedefinita");
+		this->nrMaximDeLocuri = 0;
+		this->nrLocuriOcupate = 0;
+		cout << "constr implicit - clasa Locatie" << endl;
+	}
+
+	Locatie(const char* adresa, int nrMaximDeLocuri, int nrLocuriOcupate)
+	{
+		if (adresa != nullptr && strlen(adresa) != 0)
+		{
+			this->adresa = new char[strlen(adresa) + 1];
+			strcpy(this->adresa, adresa);
+		}
+		else
+		{
+			this->adresa = nullptr;
+		}
+		if (nrMaximDeLocuri > 0)
+		{
+			this->nrMaximDeLocuri = nrMaximDeLocuri;
+		}
+		else
+		{
+			this->nrMaximDeLocuri = 0;
+		}
+		if (nrLocuriOcupate > 0)
+		{
+			this->nrLocuriOcupate = nrLocuriOcupate;
+		}
+		else
+		{
+			this->nrLocuriOcupate = 0;
+		}
+	}
+
+	Locatie(const Locatie& l)
+	{
+		if (l.adresa != nullptr && strlen(l.adresa) != 0)
+		{
+			this->adresa = new char[strlen(l.adresa) + 1];
+			strcpy(this->adresa, l.adresa);
+		}
+		else
+		{
+			this->adresa = nullptr;
+		}
+		if (l.nrMaximDeLocuri > 0)
+		{
+			this->nrMaximDeLocuri = l.nrMaximDeLocuri;
+		}
+		else
+		{
+			this->nrMaximDeLocuri = 0;
+		}
+		if (l.nrLocuriOcupate > 0)
+		{
+			this->nrLocuriOcupate = l.nrLocuriOcupate;
+		}
+		else
+		{
+			this->nrLocuriOcupate = 0;
+		}
+	}
+
+	// destructor
+	~Locatie()
+	{
+		if (this->adresa != nullptr)
+		{
+			delete[] this->adresa;
+			this->adresa = nullptr;
+		}
+	}
+
+	// operator =
+	Locatie& operator=(const Locatie& l)
+	{
+		if (this != &l)
+		{
+			if (this->adresa != nullptr)
+			{
+				delete[] this->adresa;
+				this->adresa = nullptr;
+			}
+			if (l.adresa != nullptr && strlen(l.adresa) != 0)
+			{
+				this->adresa = new char[strlen(l.adresa) + 1];
+				strcpy(this->adresa, l.adresa);
+			}
+			else
+			{
+				this->adresa = nullptr;
+			}
+			if (l.nrMaximDeLocuri > 0)
+			{
+				this->nrMaximDeLocuri = l.nrMaximDeLocuri;
+			}
+			else
+			{
+				this->nrMaximDeLocuri = 0;
+			}
+			if (l.nrLocuriOcupate > 0)
+			{
+				this->nrLocuriOcupate = l.nrLocuriOcupate;
+			}
+			else
+			{
+				this->nrLocuriOcupate = 0;
+			}
+		}
+		return *this;
+	}
+
+	// 2 Metode pentru diverse prelucrari
+	void verificareLocuri()
+	{
+		if (this->nrLocuriOcupate < this->nrMaximDeLocuri)
+		{
+			cout << "Locuri disponibile!" << endl;
+		}
+		else
+		{
+			cout << "Nu mai sunt locuri disponibile!" << endl;
+		}
+	}
+
+	void adaugareLocuri(int nrLocuri)
+	{
+		if (this->nrLocuriOcupate + nrLocuri <= this->nrMaximDeLocuri)
+		{
+			this->nrLocuriOcupate += nrLocuri;
+		}
+		else
+		{
+			cout << "Nu se pot adauga locuri!" << endl;
+		}
+	}
+
+	// getteri si setteri
+	int getNrMaximDeLocuri()
+	{
+		return this->nrMaximDeLocuri;
+	}
+
+	int getNrLocuriOcupate()
+	{
+		return this->nrLocuriOcupate;
+	}
+
+	char* getAdresa()
+	{
+		if (this->adresa != nullptr && strlen(this->adresa) != 0)
+		{
+			char* copie = new char[strlen(this->adresa) + 1];
+			strcpy(copie, this->adresa);
+			return copie;
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+
+	void setAdresa(const char* adresa)
+	{
+		if (this->adresa != nullptr)
+		{
+			delete[] this->adresa;
+			this->adresa = nullptr;
+		}
+		if (adresa != nullptr && strlen(adresa) != 0)
+		{
+			this->adresa = new char[strlen(adresa) + 1];
+			strcpy(this->adresa, adresa);
+		}
+		else
+		{
+			this->adresa = nullptr;
+		}
+	}
+
+	void setNrMaximDeLocuri(int nrMaximDeLocuri)
+	{
+		if (nrMaximDeLocuri > 0)
+		{
+			this->nrMaximDeLocuri = nrMaximDeLocuri;
+		}
+		else
+		{
+			this->nrMaximDeLocuri = 0;
+		}
+	}
+
+	void setNrLocuriOcupate(int nrLocuriOcupate)
+	{
+		if (nrLocuriOcupate > 0)
+		{
+			this->nrLocuriOcupate = nrLocuriOcupate;
+		}
+		else
+		{
+			this->nrLocuriOcupate = 0;
+		}
+	}
+
+	// supraincaracre operator <<
+	friend ostream& operator<<(ostream& out, Locatie& l)
+	{
+		out << "Adresa: " << l.adresa << endl;
+		out << "Numar maxim de locuri: " << l.nrMaximDeLocuri << endl;
+		out << "Numar locuri ocupate: " << l.nrLocuriOcupate << endl;
+		return out;
+	}
+
+	// supraincarcare operator >>
+	friend istream& operator>>(istream& in, Locatie& l)
+	{
+		cout << "Adresa: ";
+		char buffer[50];
+		in >> ws;
+		in.getline(buffer, 50);
+		if (l.adresa != nullptr)
+		{
+			delete[] l.adresa;
+			l.adresa = nullptr;
+		}
+		if (buffer != nullptr && strlen(buffer) != 0)
+		{
+			l.adresa = new char[strlen(buffer) + 1];
+			strcpy(l.adresa, buffer);
+		}
+		else
+		{
+			l.adresa = nullptr;
+		}
+		cout << "Numar maxim de locuri: ";
+		in >> l.nrMaximDeLocuri;
+		cout << "Numar locuri ocupate: ";
+		in >> l.nrLocuriOcupate;
+		return in;
+	}
+
+	// supraincarcare operator ++	
+	Locatie& operator++()
+	{
+		if (this->nrLocuriOcupate < this->nrMaximDeLocuri)
+		{
+			this->nrLocuriOcupate++;
+		}
+		else
+		{
+			cout << "Nu se poate incrementa numarul de locuri ocupate!" << endl;
+		}
+		return *this;
+	}
+
+	// supraincarcare operator --
+	Locatie& operator--()
+	{
+		if (this->nrLocuriOcupate > 0)
+		{
+			this->nrLocuriOcupate--;
+		}
+		else
+		{
+			cout << "Nu se poate decrementa numarul de locuri ocupate!" << endl;
+		}
+		return *this;
+	}
+
+};
---
+++ Proiect - Faza1/Proiect - Faza1/Source.cpp
@@ -0,0 +1,53 @@
+﻿
+// PROIECT FAZA 1 - VLAD ALIN PETRISOR, GRUPA 1061, SERIA E
+
+#include <iostream>
+#include "Bilet.h"
+#include "Eveniment.h"
+#include "Locatie.h"
+
+using namespace std;
+
+int main()
+{
+	Bilet b1;
+	Bilet b2(4, 5, 4, "Peluza");
+	Bilet b3(b2);
+	b1 = b2;
+	cout << "Returnarea id-ului constructorului implicit : " << b1.getId() << endl;
+	cout << "Returnarea tipului locului constructorului cu parametrii : " << b2.getTipLoc() << endl;
+	b2.setNrLoc(10);
+	cout << "Returnarea numarului locului dupa modificare : " << b2.getNrLoc() << endl;
+	b2.setipLoc("Tribuna");
+	cout << "Returnarea tipului locului dupa modificare : " << b2.getTipLoc() << endl;
+	b1.afisareMesaj();
+	// apel metoda
+	b2.verificareId(2);
+	Bilet b4(5, 2, 45, "VIP");
+	b4.prelucrareLoc();
+	// apel <<
+	cout << b4;
+	// apel >>
+	//cin >> b4;
+	//cout << b4;
+	
+	// apel operator ==
+	Bilet b5(3, 4, 5, "Tribuna 1");
+	Bilet b6(3, 4, 5, "Tribuna 1");
+	if (b5 == b6)
+	{
+		cout << "Biletele sunt egale" << endl;
+	}
+	else
+	{
+		cout << "Biletele nu sunt egale" << endl;
+	}
+	
+	// apel operator /
+	Bilet b7(10, 20, 30, "VIP");
+	Bilet b8 = b7 / 2;
+	cout << b8;
+
+	Eveniment e1;
+	Locatie l1;
+}
